b96eea3a7aae0cd894b2f51d724dfc2ecd889255
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2022-11-17 11:09:51 +0530

    array finished

--- Storage/storagePart2.sol
+++ Storage/storagePart2.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ComplexStorage{
+ contract ComplexStorage{
     uint256 [3] public fixedArray;
     uint256 [] public bigArray;
-    uint8 [] public smallArray;
+    uint64 [] public smallArray;
 
     mapping(uint256=>uint256) public Mymapping;
     mapping(uint256=>mapping (uint256=>uint256)) public nestedMapping;
@@ -12,6 +12,8 @@ contract ComplexStorage{
 
    constructor(){
        fixedArray=[2,67,80];
+       bigArray=[1,2,3,5,5,6,7,8];
+       smallArray=[9,8,7,6,8];
    }
 
 
@@ -20,7 +22,38 @@ contract ComplexStorage{
         assembly{
             value:=sload(add(fixedArray.slot,index))
         }
+    }
+
+    function BigArrayView(uint index) public view returns(uint256 value,bytes32 location){
+        uint Slot;
+        assembly{
+            Slot:=bigArray.slot
+        }
+        location=keccak256(abi.encode(Slot));
+        assembly{
+            value:=sload(add(location,index))
+        }
+        
     } 
+
+    function SmallArrayView(uint index) public view returns(uint256 value,bytes32 location){
+        uint Slot;
+        assembly{
+            Slot:=smallArray.slot
+        }
+        location=keccak256(abi.encode(Slot));
+        assembly{
+            value:=sload(add(location,index))
+        }
+        
+    } 
+
+    function slotView(uint _slot) public view returns(bytes32 value){
+        assembly{
+            value:=sload(_slot)
+
+        }
+    }
  
 
 
